ecaafe25ffa67704accb359ff6b36cb61dba218b
ramonvfs <ramon.vinicius.eng@gmail.com>
2026-04-09 16:31:43 -0300

    feat: implement simple main function for testing

--- plants/car/main.c
+++ plants/car/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include "../common/solver.h"
 
 // State-Space Model
 void car_dynamics(double t, double *x, double u, double *dxdt) {
@@ -7,4 +9,28 @@ void car_dynamics(double t, double *x, double u, double *dxdt) {
 
     /// dxdt[0] -> Linear Acceleration
     dxdt[0] = (u - b * x[0]) / m;
+}
+
+int main() {
+    double x = {0.0};
+    double setpoint = 100.0;
+    double t = 0.0;
+    double dt = 0.01;
+
+    double u = 0.0;           // Control Signal
+
+    while(1) {
+        double error = setpoint - x;
+
+        u = error * 3;
+
+        rk4(car_dynamics, t, &x, u, dt, 1);
+        t += dt;
+
+        printf("T: %6.2f | Vel: %6.2f m/s | Er: %6.2f\n", t, x, error);
+
+        usleep(10000);
+    }
+
+    return 0;
 }
--- plants/dc_motor/main.c
+++ plants/dc_motor/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
+#include "../common/solver.h"
 
 // State-Space Model
-void motor_dynamics(double *x, double u, double *dxdt) {
+void motor_dynamics(double t, double *x, double u, double *dxdt) {
     double R = 1.0;    // Eletric Resistance (Ohms)
     double L = 0.5;    // Eletric Inductance (H)
     double J = 0.01;   // Moment of Inertia of The Rotor (kg.m^2)
@@ -13,4 +15,28 @@ void motor_dynamics(double *x, double u, double *dxdt) {
 
     // dxdt[1] -> Derivative of Current
     dxdt[1] = (u - K * x[0] - R * x[1]) / L;
+}
+
+int main() {
+    double x[2] = {0.0, 0.0}; // Initial State
+    double setpoint = 100.0;
+    double t = 0.0;
+    double dt = 0.01;
+
+    double u = 0.0;           // Control Signal
+
+    while(1) {
+        double erro = setpoint - x[0];
+
+        u = erro * 1.2; 
+
+        rk4(motor_dynamics, t, x, u, dt, 2);
+        t += dt;
+
+        printf("T: %6.2f | Vel: %6.2f rad/s | Cor: %6.2f A | Erro: %6.2f\n", t, x[0], x[1], erro);
+        
+        usleep(10000);
+    }
+
+    return 0;
 }
