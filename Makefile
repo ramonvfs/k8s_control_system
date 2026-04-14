9aa32dd31370e589f5c8d1d0d7dc238900395b11
ramonvfs <ramon.vinicius.eng@gmail.com>
2026-04-14 15:02:05 -0300

    feat: implement model for tank

--- Makefile
+++ Makefile
@@ -2,13 +2,16 @@ CC = gcc
 CFLAGS = -Wall -O2
 LIBS = -lcurl
 
-all: dc_motor car
+all: dc_motor car tank
 
 dc_motor: plants/dc_motor/main.c plants/common/solver.c plants/common/network.c
 	$(CC) $(CFLAGS) -o dc_motor plants/dc_motor/main.c plants/common/solver.c plants/common/network.c $(LIBS)
 
 car: plants/car/main.c plants/common/solver.c plants/common/network.c
 	$(CC) $(CFLAGS) -o car plants/car/main.c plants/common/solver.c plants/common/network.c $(LIBS)
- 
+
+tank: plants/tank/main.c plants/common/solver.c plants/common/network.c
+	$(CC) $(CFLAGS) -o tank plants/tank/main.c plants/common/solver.c plants/common/network.c $(LIBS) -lm
+
 clean:
-	rm -f dc_motor car
+	rm -f dc_motor car tank
---
+++ plants/tank/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include "../common/network.h"
+#include "../common/solver.h"
+
+void tank_dynamics(double t, double *x, double u, double *dxdt) {
+    double A = 2.0;      // Tank cross-sectional area (m2)
+    double a = 0.05;     // Outlet tube cross-sectional area (m2)
+    double qm = 0.1;     // Maximum pump flow rate (m3/s)
+    double g = 9.81;     // Acceleration due to gravity (m/s2)
+    double Vo = 0.5;     // Outlet valve opening (0.0 to 1.0)
+
+    // x[0] represents the water level n(t)
+    // Ensure level is not negative due to numerical errors in sqrt
+    double level = (x[0] > 0) ? x[0] : 0;
+
+    // dxdt[0] -> Derivative of the level (dn/dt)
+    dxdt[0] = (qm * u - Vo * a * sqrt(2 * g * level)) / A;
+}
+
+int main() {
+    double x[1] = {0.5};
+    double setpoint = 2.0;
+    double t = 0.0;
+    double dt = 0.01;
+
+    double v_n = 0.0;           // Control Signal
+    double error = 0;
+    double prev_error = 0;
+    double s_n = 0.0;
+
+    double ki = 0.1;
+    double kp = 0.05;
+
+    while(1) {
+        error = setpoint - x[0];
+
+        s_n = call_fuzzy_controller(error * ki, (error - prev_error) * kp);
+
+        v_n = v_n + s_n;
+
+        /* Valve Saturation
+        if (v_n > 1.0) v_n = 1.0; 
+        if (v_n < 0.0) v_n = 0.0;
+        */
+
+        prev_error = error;
+
+        rk4(tank_dynamics, t, x, v_n, dt, 1);
+        t += dt;
+
+        printf("T: %6.2f | N: %6.2f m | Er: %6.2f | U: %6.2f\n", t, x[0], error, v_n);
+
+        usleep(10000);
+    }
+
+    return 0;
+}
